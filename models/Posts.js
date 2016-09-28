8e76e735d2288b67b96cbe7d84b7bf266f80eb71
Ryan Zell <ryanzell36@gmail.com>
2016-09-27 21:50:23 -0700

    Add Author to Posts

--- models/Posts.js
+++ models/Posts.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var PostSchema = new mongoose.Schema({
   title: String,
   link: String,
+  author: String,
   upvotes: {type: Number, default: 0},
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 });
